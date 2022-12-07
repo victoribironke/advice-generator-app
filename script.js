136df3f255f8530e73ed1a7ef55775e75257539a
Victor Ibironke <82767685+victoribironke@users.noreply.github.com>
2022-12-07 20:18:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 w3.getHttpObject("https://api.adviceslip.com/advice", (data) => {
+  document.querySelector(".no").textContent = `Advice #${data.slip.id}`;
   document.querySelector(".advice").textContent = `"${data.slip.advice}"`;
 });
 
 document.querySelector(".img").addEventListener("click", () => {
   w3.getHttpObject("https://api.adviceslip.com/advice", (data) => {
+    document.querySelector(".no").textContent = `Advice #${data.slip.id}`;
     document.querySelector(".advice").textContent = `"${data.slip.advice}"`;
   });
 });
