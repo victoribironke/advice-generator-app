10d45222e9ad15e56a0fd0499bb453df5c38cbcd
Victor Ibironke <82767685+victoribironke@users.noreply.github.com>
2023-02-26 23:17:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-w3.getHttpObject("https://api.adviceslip.com/advice", (data) => {
-  document.querySelector(".no").textContent = `Advice #${data.slip.id}`;
-  document.querySelector(".advice").textContent = `"${data.slip.advice}"`;
-});
-
-document.querySelector(".img").addEventListener("click", () => {
+const getAdvice = () => {
   w3.getHttpObject("https://api.adviceslip.com/advice", (data) => {
     document.querySelector(".no").textContent = `Advice #${data.slip.id}`;
     document.querySelector(".advice").textContent = `"${data.slip.advice}"`;
   });
-});
+};
+
+document.querySelector(".img").addEventListener("click", getAdvice);
+getAdvice();
